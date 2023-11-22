b1e41f3c5e1e06e8c09a447c938efb484000ef0a
Glitch (glitch-hello-website) <none>
2023-11-22 07:58:54 +0000

    🎄⛩ Checkpoint ./index.html:44704417/64649 ./chat.js:44704417/2703

--- chat.js
+++ chat.js
@@ -1,47 +1,47 @@
-const SERVER_URL = window.location.origin.includes('localhost')
-	? 'http://localhost:5000'
-	: 'https://holonext-ai-assistant.onrender.com';
+// const SERVER_URL = window.location.origin.includes('localhost')
+const SERVER_URL = "http://localhost:5000";
 
+const processAudio = async (recordingBlob) => {
+  const formData = new FormData();
+  formData.append("audio", recordingBlob);
 
-	const processAudio = async (recordingBlob) => {
-		const formData = new FormData();
-		formData.append('audio', recordingBlob);
+  try {
+    const response = await axios.post(
+      `${SERVER_URL}/api/speech-to-text`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
 
-		try {
-			const response = await axios.post(`${SERVER_URL}/api/speech-to-text`, formData, {
-				headers: {
-					'Content-Type': 'multipart/form-data',
-				},
-			});
-
-      return response.data.text
-		} catch (error) {
-			console.log('error', error);
-		}
+    return response.data.text;
+  } catch (error) {
+    console.log("error", error);
   }
+};
 
+const getChatGPTResponse = async (transcript) => {
+  console.log("sending transcript : ", transcript);
+  try {
+    const response = await axios.post(`${SERVER_URL}/api/chat`, {
+      message: transcript,
+    });
 
+    const answer = response.data.answer || "How can ı help you?";
+    const answerAudio = response.data.audio;
 
-const getChatGPTResponse =async (transcript) => {
-		console.log('sending transcript : ', transcript);
-		try {
-			const response = await axios.post(`${SERVER_URL}/api/chat`, {
-				message: transcript,
-			});
+    console.log("answer", answer);
 
-			const answer = response.data.answer || 'How can ı help you?';
-			const answerAudio = response.data.audio;
+    // convert audio buffer to blob
+    const buffer = new Uint8Array(answerAudio.data);
+    const blob = new Blob([buffer], { type: "audio/mpeg" });
+    const audioData = new File([blob], "answer.mp3", { type: "audio/mpeg" });
 
-			console.log('answer', answer);
-
-			// convert audio buffer to blob
-			const buffer = new Uint8Array(answerAudio.data);
-			const blob = new Blob([buffer], { type: 'audio/mpeg' });
-			const audioData = new File([blob], 'answer.mp3', { type: 'audio/mpeg' });
-
-			const audio = new Audio(URL.createObjectURL(audioData));
-			audio.play();
-		} catch (error) {
-			console.log('error', error);
-		}
-	}
+    const audio = new Audio(URL.createObjectURL(audioData));
+    audio.play();
+  } catch (error) {
+    console.log("error", error);
+  }
+};
